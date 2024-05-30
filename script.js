35997c55bbe7ec8fadbe1cf2c2a20bce12399924
Nitolez <antonioglezt98@gmail.com>
2024-05-30 13:38:23 +0200

    Rehecha con la async

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ const tablaMatricula = document.querySelector('#tabla')
 const formulario = document.querySelector('#buscarMatricula')
 const listaErrores = document.querySelector('#listaErrores')
 const regExp = /^([A-Z]{0,1}|([A-Z]-){0,1})\d{4}-[A-Z]{1,3}$/
+let arrayFinal = []
 
-
-const arrayConductores = [
+const arrayCoches = [
     {
         matricula: '7171-SOY',
         modelo: 'Dibujo animado',
@@ -31,13 +31,31 @@ const arrayConductores = [
         multas: []
     }
 ]
-
+const arrayMultas = [
+    {
+        matricula: '7171-SOY',
+        multas: ['Multa por exceso de velocidad', 'Multa por sexy']
+    },
+    {
+        matricula: '6712-UNA',
+        multas: ['Multa por gritar a Stephani']
+    },
+    {
+        matricula: '0008-MIE',
+        multas: ['Multa por robar', 'Multa por incendio', 'Multa por venta de drogas', 'Multa por matar a Ash']
+    },
+    {
+        matricula: '3672-RDA',
+        multas: []
+    }
+]
 
 //EVENTOS
 
 formulario.addEventListener("submit", (ev) => {
     ev.preventDefault()
-    validarMatricula()
+    listaErrores.innerHTML='';
+    getInfoMultado()
 })
 
 
@@ -45,52 +63,51 @@ formulario.addEventListener("submit", (ev) => {
 
 //FUNCIONES
 
-const validarMatricula = async () => {
-
-    const matriculaEscrita = document.querySelector("#ingresoMatricula").value
-    let errores = '';
-    let encontrarCoche = arrayConductores.find(coche => coche.matricula === matriculaEscrita)
-    /*localStorage.setItem('encontrarCoche', JSON.stringify(encontrarCoche))
-    encontrarCoche = JSON.parse(localStorage.getItem("encontrarCoche"))*/
-
-
-    if (matriculaEscrita === '') {
-        errores = '<li>Escribe la matrícula</li>'
-    } else if (regExp.test(matriculaEscrita) === false) {
-        errores = '<li>Formato de matrícula no admitido</li>'
-    } else if (!encontrarCoche) {
-        errores = '<li>No existe la matrícula</li>'
-    } else if (!Array.isArray(encontrarCoche.multas) || encontrarCoche.multas.length === 0) {
-        errores = '<li>No hay multas para este coche</li>'
-    }
-
-    if (errores !== '') {
-        listaErrores.innerHTML = errores
-    } else {
-        listaErrores.innerHTML = '';
-        agregarATabla(encontrarCoche.matricula, encontrarCoche.modelo, encontrarCoche.propietario, encontrarCoche.multas.join(', '))
-    }
-
+const validarMatricula = async () =>{
+    const matricula = document.querySelector("#ingresoMatricula").value        
+    if(regExp.test(matricula)) return matricula
+        else throw listaErrores.innerHTML = 'Formato de matrícula incorecto.'
 }
 
-const agregarATabla = async (matricula, modelo, propietario, multas) => {
-    const fila = document.createElement('tr')
-
-    const celdaMatricula = document.createElement("td")
-    celdaMatricula.textContent = matricula
-    fila.append(celdaMatricula)
-
-    const celdaModelo = document.createElement("td")
-    celdaModelo.textContent = modelo
-    fila.append(celdaModelo)
-
-    const celdaPropietario = document.createElement("td")
-    celdaPropietario.textContent = propietario
-    fila.append(celdaPropietario)
-
-    const celdaMultas = document.createElement("td")
-    celdaMultas.textContent = multas
-    fila.append(celdaMultas)
-
-    tablaMatricula.append(fila)
+// Funcion validarCoche
+const validarCoche = async(matricula) => {
+    let cocheValidado = arrayCoches.find((elemento) => elemento.matricula === matricula);
+    let arrayFinal = {...cocheValidado}
+    if(cocheValidado) return arrayFinal
+    else throw listaErrores.innerHTML = 'Este coche no esta registrado.'
 }
+
+// Función validarMultas
+const validarMultas = async(arrayFinal) => {
+    const multaValidada = arrayMultas.find((elemento) => elemento.matricula === arrayFinal.matricula);
+    if(multaValidada) {
+        arrayFinal.multas = multaValidada.multas.length;
+        return arrayFinal
+    }
+    else throw listaErrores.innerHTML = `El coche con matricula ${arrayFinal.matricula} no tiene multas`
+};
+// Función pintarMultas
+const pintarMultas = (arrayFinal => {
+    const trBody = document.createElement('tr');
+    const tdMatricula = document.createElement('td');
+    const tdModelo = document.createElement('td');
+    const tdPropietario = document.createElement('td');
+    const tdMultas = document.createElement('td');
+    tdMatricula.textContent = arrayFinal.matricula;
+    tdModelo.textContent = arrayFinal.modelo;
+    tdPropietario.textContent = arrayFinal.propietario;
+    tdMultas.textContent = arrayFinal.multas;
+    trBody.append(tdMatricula, tdModelo, tdPropietario, tdMultas);
+    tablaMatricula.append(trBody);
+});
+// Función madre
+const getInfoMultado=async()=>{
+    try{
+      const matricula = await validarMatricula();
+      const arrayFinal = await validarCoche(matricula);
+      const arrayFinal2 = await validarMultas(arrayFinal);
+      pintarMultas(arrayFinal2);
+    }catch(error){
+    error
+    }
+};
